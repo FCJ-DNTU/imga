6672daa796fb49f56981e6cafc28417770c1fb66
Nguyen Anh Tuan <nguyenanhtuan19122002@gmail.com>
2024-08-15 16:14:56 +0700

    Update docker installation script

--- docker_install.sh
+++ docker_install.sh
@@ -6,6 +6,11 @@ sudo yum update -y
 ## Install docker
 sudo yum install -y docker
 
+## Start docker daemon
+sudo usermod -a -G docker ec2-user
+## Or
+# sudo systemctl start docker
+
 ## Add user to docker
 sudo usermod -d -G docker ec2-user
 
