81d0867acc0350152934bdca985a6b29b716573e
Nguyen Anh Tuan <nguyenanhtuan19122002@gmail.com>
2024-08-18 08:37:34 +0700

    Update script and dockerfile

--- backend/install.sh
+++ backend/install.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
+. ./utils.sh
+
 # Change directory to `backend`
+infoln "Change directory to /home/backend"
 cd /home/backend
 
 # Install NodeJS Server
+infoln "Installing NodeJS Dependencies..."
 bash nodejs/scripts/install.sh
+infoln "Done!"
 
 # Change directory to `backend`
 cd /home/backend
 
 # Install Python
+infoln "Installing Python Packages, Tesseract OCR..."
 bash python/scripts/install.sh
+infoln "Done!"
 
 # Change directory to `backend`
 cd /home/backend
 
 # Start server
+infoln "Booting server..."
 bash nodejs/scripts/start.sh
+infoln "Done!"
