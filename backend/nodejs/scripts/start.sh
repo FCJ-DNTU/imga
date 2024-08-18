d46f7e3f85b2ae67a63ebf1db7dc82435774c8fd
Nguyen Anh Tuan <nguyenanhtuan19122002@gmail.com>
2024-08-18 15:04:31 +0700

    Update backend/nodejs/scripts/start.sh

--- backend/nodejs/scripts/start.sh
+++ backend/nodejs/scripts/start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Set up PATH
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+
 # Change directory to `nodejs`
 cd /home/backend/nodejs
 
